1273b35cad414aa4f9933aabd0b987026b90bb51
Steff Brouwer <sbrouwer05@gmail.com>
2023-11-26 23:24:15 +0100

    config

---
+++ app/config/config.php
@@ -0,0 +1,7 @@
+<?php
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'mvc-2209a-p4');
+define('URLROOT', 'http://www.be-j2-opdracht3.org/');
